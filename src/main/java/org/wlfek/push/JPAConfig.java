0cc6b3a1c1ed1873849112f17cf87c59dedcd320
13011010 <13011010@SEC-5228-0075.sds.shinhan.com>
2016-04-06 16:16:52 +0900

    JPA Config class 설정

--- src/main/java/org/wlfek/push/JPAConfig.java
+++ src/main/java/org/wlfek/push/JPAConfig.java
@@ -3,7 +3,9 @@ package org.wlfek.push;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
@@ -16,6 +18,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+// application.properties 보다 우선시 됨.
 @Configuration
 @EnableTransactionManagement
 public class JPAConfig {
@@ -24,29 +27,49 @@ public class JPAConfig {
 //    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
 //        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 //        em.setDataSource(dataSource());
-//        em.setPackagesToScan("net.woniper.jpa.domain");
-// 
-//        // persistence 설정
-//        Properties properties = new Properties();
-//        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-//        properties.setProperty("hibernate.show_sql", "true");
-// 
+//        em.setPackagesToScan("org.wlfek.push");
 //        // 각 구현체의 프로퍼티 확장 및 설정
 //        JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
 //        em.setJpaVendorAdapter(jpaVendorAdapter);
-//        em.setJpaProperties(properties);
+//        em.setJpaProperties(additionalJpaProperties());
 //        return em;
+//    }
+//    
+//    /**
+//     * 각 구현체의 프로퍼티 확장 및 설정
+//     * @return Properties
+//     */
+//    private Properties additionalJpaProperties(){
+//        Properties properties = new Properties();
+//        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+//        properties.setProperty("hibernate.generate-ddl", "false");
+//        properties.setProperty("hibernate.show_sql", "true");
+//        properties.setProperty("hibernate.naming-strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
+//        return properties;
 //    }
 // 
 //    // H2 DB 설정
-//    @Bean(destroyMethod = "shutdown")
-//    public EmbeddedDatabase dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .setName("slipp")
-//                .build();
-//    }
+////    @Bean(destroyMethod = "shutdown")
+////    public EmbeddedDatabase dataSource() {
+////        return new EmbeddedDatabaseBuilder()
+////                .setType(EmbeddedDatabaseType.H2)
+////                .setName("slipp")
+////                .build();
+////    }
 // 
+//    
+//    @Bean
+//    public DataSource dataSource(){
+//    	DataSourceBuilder.create().findType();
+//        return DataSourceBuilder.create()
+//        		.url("jdbc:mariadb://127.0.0.1:3306/push")
+//                .driverClassName("org.mariadb.jdbc.Driver")
+//                .username("root")
+//                .password("dlfhdia33")
+//                .build();
+//    }   
+//    
+//    
 //    // Transaction 설정
 //    @Bean
 //    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
